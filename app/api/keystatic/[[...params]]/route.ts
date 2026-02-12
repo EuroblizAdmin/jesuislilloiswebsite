908a0fa0c1cc02306785118f305dee27df48a188
saswat-53 <saswat53@gmail.com>
2026-02-12 13:03:34 +0530

    Add GitHub OAuth credentials to Keystatic API route
    Configure GitHub client ID and secret for production authentication in the Keystatic route handler to enable GitHub storage mode.

--- app/api/keystatic/[[...params]]/route.ts
+++ app/api/keystatic/[[...params]]/route.ts
@@ -4,4 +4,6 @@ import config from '../../../../keystatic.config';
 export const { GET, POST } = makeRouteHandler({
   config,
   localBaseDirectory: process.cwd(),
+  clientId: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
 });
